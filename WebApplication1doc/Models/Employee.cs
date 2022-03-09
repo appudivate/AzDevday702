a904606dc94917086503a2756ac0db49b6d636bb
Divate <2085120@cognizant.com>
2022-03-09 21:22:18 +0530

    commi message

---
+++ WebApplication1doc/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1doc.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeFName { get; set; }
+
+        public string Job { get; set; }
+    }
+}
